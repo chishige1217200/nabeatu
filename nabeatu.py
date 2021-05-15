3455df29906e53dda3e834d8e62e6e40766560f6
AAAR-Salmon <tenzikujohn@gmail.com>
2021-05-15 23:36:35 +0900

    [refactor] Falseを返す
    return文に何もつけなければNoneが返り，
    これは確かにFalsyですがFalseを明示したほうが可読性が高いと思われます
    
    ついでにif文の `== True` を取り除きました

--- nabeatu.py
+++ nabeatu.py
@@ -1,16 +1,15 @@
 def nabeatu(num: int):
     if num <= 0:
-        return
+        return False
     if num % 3 == 0:
         return True
 
     s = str(num)
-    for di in s:
-        if int(di) == 3:
-            return True
+
+    return False
 
 
 # main
 for num in range(0, 401):
-    if nabeatu(num) == True:
+    if nabeatu(num):
         print(str(num))
